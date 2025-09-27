92fcf6d7b5231152066d1b913a2121e1508a8837
Rovda_V <v_rovda@mail.ru>
2025-09-27 18:34:39 +0300

    Убрал var

--- public/components/Fact_Bal/index.js
+++ public/components/Fact_Bal/index.js
@@ -1,8 +1,8 @@
 export class FactBal {
     getSelf(FactSum, PrevFactSum,updateFactFortwoWeek) {
         const template = Handlebars.templates["FactBalance"];
-        var informationsAboutSign = updateFactFortwoWeek > 0;
-        var procents = Math.abs(FactSum / (PrevFactSum / 100) - 100).toFixed(1);
+        let informationsAboutSign = updateFactFortwoWeek > 0;
+        let procents = Math.abs(FactSum / (PrevFactSum / 100) - 100).toFixed(1);
         return template ({
             FactSum : FactSum,
             updateFactFortwoWeek : updateFactFortwoWeek,
