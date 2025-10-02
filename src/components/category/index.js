89fc37624aa077e866895f7e5ad430858f52b7ec
Imperialmelon <bogart30@yandex.ru>
2025-10-02 09:00:38 +0300

    chart-circle login style fixes

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -2,10 +2,16 @@ import Handlebars from "handlebars";
 import CategoryTemplate from "../../templates/components/Category.hbs?raw";
 
 export class Category {
+<<<<<<< HEAD
   constructor() {
     this.template = Handlebars.compile(CategoryTemplate);
   }
   getSelf(category, category_name) {
     return this.template({ category, category_name });
+=======
+  getSelf(color, text_color,  category_name) {
+    const template = Handlebars.templates["Category"];
+    return template({ color, text_color, category_name });
+>>>>>>> 27fa6ce (chart-circle login style fixes)
   }
 }
