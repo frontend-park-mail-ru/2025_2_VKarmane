324a36f66af58d33b951a37a4be8d0bdc8c3a70d
Imperialmelon <bogart30@yandex.ru>
2025-10-08 21:12:39 +0300

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 export class Router {
+<<<<<<< HEAD
   routes: Record<string, () => void>;
   constructor(routes: Record<string, () => void>) {
     this.routes = routes;
@@ -17,4 +18,26 @@ export class Router {
     if (route) route();
   }
 }
+<<<<<<< HEAD
 
+=======
+=======
+    routes: Record<string, () => void>;
+    constructor(routes: Record<string, () => void>) {
+        this.routes = routes;
+        window.addEventListener("popstate", () => this.loadRoute());
+    }
+
+    navigate(path: string) {
+        history.pushState({}, "", path);
+        this.loadRoute();
+    }
+
+    loadRoute() {
+        const path = window.location.pathname;
+        const route = this.routes[path] || this.routes["*"];
+        if (route) route()
+    }
+}
+>>>>>>> deea218 (router)
+>>>>>>> 70dd6d5 (router)
