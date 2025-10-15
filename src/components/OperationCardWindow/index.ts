1badf34c2a75a368cc50128baf5dc5bac93bb942
Rovda_V <v_rovda@mail.ru>
2025-10-15 03:40:03 +0300

    added ts script to OperationCardWindow

--- src/components/OperationCardWindow/index.ts
+++ src/components/OperationCardWindow/index.ts
@@ -1,15 +1,25 @@
 import Handlebars from "handlebars";
 import TransactionsListTemplate from "../../templates/components/transactionsList.hbs?raw";
 
+export interface OperationItem {
+    id: string | number;
+    date?: string;
+    amount?: number;
+    description?: string;
+    [key: string]: any;
+}
+
 export class TransactionsList {
+    private template: Handlebars.TemplateDelegate;
+
     constructor() {
         this.template = Handlebars.compile(TransactionsListTemplate);
     }
-    getList(operationsArray) {
+
+    public getList(operationsArray: OperationItem[]): string {
         return this.template({
             operations_exists: operationsArray.length > 0,
             operationsItems: operationsArray,
         });
     }
-
 }
