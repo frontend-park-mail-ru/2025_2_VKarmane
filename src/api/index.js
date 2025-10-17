3704017c5173057ad980a45d3f6ee0bd247a50df
Rovda_V <v_rovda@mail.ru>
2025-10-17 12:27:30 +0300

    Случайно удалил файлы, вернул

---
+++ src/api/index.js
@@ -0,0 +1,33 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+async function fetchWithAuth(url, options = {}) {
+    const headers = {
+        "Content-Type": "application/json",
+        ...options.headers,
+    };
+
+    const response = await fetch(API_URL + url, {
+        ...options,
+        headers,
+        // credentials: "include",
+    });
+    if (!response.ok)
+        throw new Error(`Ошибка при запросе ${url}: ${response.status}`);
+    return await response.json();
+}
+
+export function getBalance() {
+    return fetchWithAuth(`/balance`);
+}
+
+export function getBalanceById(id) {
+    return fetchWithAuth(`/balance/${id}`);
+}
+
+export function getBudgets() {
+    return fetchWithAuth(`/budgets`);
+}
+
+export function getBudgetById(id) {
+    return fetchWithAuth(`/budgets/${id}`);
+}
